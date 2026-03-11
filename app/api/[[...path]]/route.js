cc2a48a4048a8c5532c6da1c62cc8395fa60414a
abmd4694-del <abmd4694@gmail.com>
2026-03-11 20:17:21 +0530

    fix: make Stripe initialization build-safe in Vercel

--- app/api/[[...path]]/route.js
+++ app/api/[[...path]]/route.js
@@ -7,7 +7,7 @@ import Stripe from 'stripe'
 let client
 let db
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || 'sk_test_fallback')
 
 async function connectToMongo() {
   if (!client) {
